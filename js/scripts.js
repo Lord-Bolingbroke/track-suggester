2425decda05babc466b4f0ad5a46f2f426fef21a
Noah Schooler <noahschooler1@gmail.com>
2018-10-19 12:50:39 -0700

    add branching for Ruby/Rails option

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,28 @@ $(document).ready(function() {
 
     var companySize = $("#companySize").val();
     var brainType = $("#brainType").val();
-    var backEnd = $("#backEnd").val();
+    var end = $("#end").val();
     var mobile = $("#mobile").val();
     var popularity = $("#popularity").val();
     debugger;
 
-    if (companySize === "Please select an option" || brainType === "Please select an option" || backEnd === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
+    if (companySize === "Please select an option" || brainType === "Please select an option" || end === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
       $("#results").text("Please fill out all fields.")
-    } else if (brainType === "Right-brained") {
+    } else if (companySize === "Smaller company" && end === "Web development" mobile === "Yes") {
+      $("#results").text("Ruby/Rails")
+    } else if (brainType === "Right-brained" && end === "Web development") {
       $("#results").text("CSS/design")
+    } else if (mobile === "Yes" && companySize === "Larger company") {
+      $("#results").text("Java/Android")
     } else if (companySize === "Larger company" && popularity === "More popular") {
       $("#results").text("C#/.NET")
-    } else {
+    } else if (end === "Creating back-end applications") {
+      $("#results").text("PHP/Drupal")
+    } else if (mobile === "Yes" && companySize === "Larger company") {
+      $("#results").text("Java/Android")
+    }
+
+    else {
       $("#results").text("Sorry, we could not find a track for you based on the information you inputted.")
     }
 
