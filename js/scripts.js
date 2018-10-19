c25d40b11368cb38d4d34eb5fb7a21d58df010d3
Noah Schooler <noahschooler1@gmail.com>
2018-10-19 11:58:55 -0700

    add more branching for C#/.NET option

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#lengths").submit(function(event) {
+  $("#survey").submit(function(event) {
     event.preventDefault();
 
     var companySize = $("#companySize").val();
@@ -7,7 +7,7 @@ $(document).ready(function() {
     var backEnd = $("#backEnd").val();
     var mobile = $("#mobile").val();
     var popularity = $("#popularity").val();
-
+debugger;
     // companySize
     if (companySize === "Larger company" && popularity === "More popular") {
       $("#results").text("C#/.NET")
