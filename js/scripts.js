b46524d6d8f87bb85e4dd7d85218e915009ce463
Noah Schooler <noahschooler1@gmail.com>
2018-10-19 12:19:47 -0700

    add JavaScript that prompts users to fill out all survey fields

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,14 @@ $(document).ready(function() {
     var backEnd = $("#backEnd").val();
     var mobile = $("#mobile").val();
     var popularity = $("#popularity").val();
-debugger;
+    debugger;
     // companySize
-    if (companySize === "Larger company" && popularity === "More popular") {
+    if (companySize === "Please select an option" || brainType === "Please select an option" || backEnd === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
+      $("#results").text("Please fill out all fields.")
+    } else if (companySize === "Larger company" && popularity === "More popular") {
       $("#results").text("C#/.NET")
+    } else {
+      $("#results").text("Sorry, we could not find a track for you based on the information you inputted.")
     }
 
 
