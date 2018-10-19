a80112cd048efc22c90d89c988e414f6d4bac7df
Noah Schooler <noahschooler1@gmail.com>
2018-10-19 15:17:54 -0700

    add semi-functioning JavaScript to display name and track in a results div

--- js/scripts.js
+++ js/scripts.js
@@ -12,33 +12,31 @@ $(document).ready(function() {
     if (name === "" || companySize === "Please select an option" || brainType === "Please select an option" || end === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
       $("#results").text("Please fill out all fields.")
     } else if (companySize === "smaller" && end === "web" && mobile === "Yes") {
-      $("#results").text("Ruby/Rails")
-      $("#nameInsert").append(name)
-      $("#track5").show()
+      $("#nameInsert").show(name)
+      $("#results").show()
+      $("#track").text("Ruby/Rails")
+      $("#nameInsert").show(name)
     } else if (brainType === "Right-brained" && end === "web") {
-      $("#results").text("CSS/design")
-      $("#nameInsert").append(name)
-      $("#track1").show()
+      $("#track").text("CSS/design")
+      $("results").show()
     } else if (mobile === "Yes" && companySize === "larger") {
-      $("#results").text("Java/Android")
-      $("#nameInsert").append(name)
-      $("#track3").show()
+      $("#nameInsert").show(name)
+      $("#results").show()
+      $("#track").text("Java/Android")
     } else if (companySize === "larger" && popularity === "More popular") {
-      $("#results").text("C#/.NET")
-      $("#nameInsert").append(name)
-      $("#track2").show()
+      $("#nameInsert").show(name)
+      $("#results").show()
+      $("#track").text("C#/.NET")
     } else if (end === "back-end") {
-      $("#results").text("PHP/Drupal")
-      $("#nameInsert").append(name)
-      $("#track4").show()
-      debugger;
+      $("#nameInsert").show(name)
+      $("#results").show()
+      $("#track").text("PHP/Drupal")
     } else if (mobile === "Yes" && companySize === "larger") {
-      $("#results").text("Java/Android")
-      $("#nameInsert").append(name)
-      $("#track3").show()
+      $("#nameInsert").show(name)
+      $("#results").show()
+      $("#track").text("Java/Android")
     } else {
-      $("#results").text("Sorry, we could not find a track for you based on the information you provided.")
+      $("#track").text("Sorry, we could not find a track for you based on the information you provided.")
     }
-debugger;
   });
 });
