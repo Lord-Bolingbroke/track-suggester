444c2eabc557f8a7ab6bb23c11cf9930cdeb9175
Noah Schooler <noahschooler1@gmail.com>
2018-10-19 13:35:14 -0700

    add JavaScript to display the input of the name field in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,29 @@ $(document).ready(function() {
     var end = $("#end").val();
     var mobile = $("#mobile").val();
     var popularity = $("#popularity").val();
+    var name = $("#name").val();
 
-    if (companySize === "Please select an option" || brainType === "Please select an option" || end === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
+    if (name === "" || companySize === "Please select an option" || brainType === "Please select an option" || end === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
       $("#results").text("Please fill out all fields.")
     } else if (companySize === "smaller" && end === "web" && mobile === "Yes") {
       $("#results").text("Ruby/Rails")
+      $("#nameInsert").append(name)
     } else if (brainType === "Right-brained" && end === "web") {
       $("#results").text("CSS/design")
+      $("#nameInsert").append(name)
     } else if (mobile === "Yes" && companySize === "larger") {
       $("#results").text("Java/Android")
+      $("#nameInsert").append(name)
     } else if (companySize === "larger" && popularity === "More popular") {
       $("#results").text("C#/.NET")
     } else if (end === "back-end") {
       $("#results").text("PHP/Drupal")
+      $("#nameInsert").append(name)
     } else if (mobile === "Yes" && companySize === "larger") {
       $("#results").text("Java/Android")
+      $("#nameInsert").append(name)
     } else {
-      $("#results").text("Sorry, we could not find a track for you based on the information you inputted.")
+      $("#results").text("Sorry, we could not find a track for you based on the information you provided.")
     }
 
   });
