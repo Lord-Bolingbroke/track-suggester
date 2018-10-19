5c278114b972229884977e2a5629a4c9b6393d5e
Noah Schooler <noahschooler1@gmail.com>
2018-10-19 13:02:05 -0700

    add custom CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,24 @@ $(document).ready(function() {
     var end = $("#end").val();
     var mobile = $("#mobile").val();
     var popularity = $("#popularity").val();
-    debugger;
 
     if (companySize === "Please select an option" || brainType === "Please select an option" || end === "Please select an option" || mobile === "Please select an option" || popularity === "Please select an option") {
       $("#results").text("Please fill out all fields.")
-    } else if (companySize === "Smaller company" && end === "Web development" mobile === "Yes") {
+    } else if (companySize === "smaller" && end === "web" && mobile === "Yes") {
       $("#results").text("Ruby/Rails")
-    } else if (brainType === "Right-brained" && end === "Web development") {
+    } else if (brainType === "Right-brained" && end === "web") {
       $("#results").text("CSS/design")
-    } else if (mobile === "Yes" && companySize === "Larger company") {
+    } else if (mobile === "Yes" && companySize === "larger") {
       $("#results").text("Java/Android")
-    } else if (companySize === "Larger company" && popularity === "More popular") {
+    } else if (companySize === "larger" && popularity === "More popular") {
       $("#results").text("C#/.NET")
-    } else if (end === "Creating back-end applications") {
+    } else if (end === "back-end") {
       $("#results").text("PHP/Drupal")
-    } else if (mobile === "Yes" && companySize === "Larger company") {
+    } else if (mobile === "Yes" && companySize === "larger") {
       $("#results").text("Java/Android")
-    }
-
-    else {
+    } else {
       $("#results").text("Sorry, we could not find a track for you based on the information you inputted.")
     }
-
-
 
   });
 });
